611a032b7a241886799c2c2f6da15fd4d0d2a187
Abhisheksunda2000 <110471724+Abhisheksunda2000@users.noreply.github.com>
2023-07-24 08:01:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,168 @@
+// app.js
+const MY_API_KEY = '1172b46a';
+const BASE_URL = 'https://www.omdbapi.com/';
+const moviesPerPage = 12;
+let currentPageNum = 1;
+let totalMovies = 0;
+let currentMoviesData = [];
+let selectedMovieId = null;
+
+function showLoadingSpinner() {
+  const movieListDiv = document.getElementById("movieList");
+  const spinnerDiv = document.createElement("div");
+  spinnerDiv.className = "spinner";
+  movieListDiv.innerHTML = ""; // Clear movie list
+  movieListDiv.appendChild(spinnerDiv);
+}
+
+function hideLoadingSpinner() {
+  const movieListDiv = document.getElementById("movieList");
+  movieListDiv.innerHTML = ""; // Clear movie list
+}
+
+function displayError(errorMessage) {
+  const movieListDiv = document.getElementById("movieList");
+  const errorDiv = document.createElement("div");
+  errorDiv.textContent = errorMessage;
+  errorDiv.style.color = "red";
+  movieListDiv.innerHTML = ""; // Clear movie list
+  movieListDiv.appendChild(errorDiv);
+}
+
+function handleMovieData(data) {
+  if (data.Response === "True") {
+    totalMovies = parseInt(data.totalResults);
+    currentMoviesData = data.Search;
+    displayMovies();
+  } else {
+    const errorMessage = data.Error || "No movies found!";
+    displayError(errorMessage);
+  }
+}
+
+async function fetchMovies(keyword) {
+  try {
+    showLoadingSpinner();
+    const response = await fetch(
+      `${BASE_URL}?s=${keyword}&apikey=${MY_API_KEY}&page=${currentPageNum}`
+    );
+    const data = await response.json();
+    hideLoadingSpinner();
+    handleMovieData(data);
+  } catch (error) {
+    hideLoadingSpinner();
+    displayError("Error fetching movie data. Please try again later.");
+    console.error("Error fetching movie data:", error);
+  }
+}
+
+function displayMovies() {
+  const movieListDiv = document.getElementById("movieList");
+  movieListDiv.innerHTML = "";
+
+  for (const movie of currentMoviesData) {
+    const movieDiv = document.createElement("div");
+    movieDiv.className = "movie";
+
+    const posterImg = document.createElement("img");
+    posterImg.src = movie.Poster;
+    posterImg.alt = movie.Title;
+    posterImg.width = 100;
+    posterImg.height = 200;
+    posterImg.style.objectFit = "cover";
+
+    const movieTitle = document.createElement("h3");
+    movieTitle.textContent = movie.Title;
+
+    movieDiv.appendChild(posterImg);
+    movieDiv.appendChild(movieTitle);
+
+    movieDiv.addEventListener("click", () => openMovieDetails(movie));
+    movieListDiv.appendChild(movieDiv);
+  }
+
+  document.getElementById("currentPageNum").textContent = currentPageNum;
+}
+
+function searchMovies() {
+  const keyword = document.getElementById("searchInput").value.trim();
+  if (keyword) {
+    currentPageNum = 1;
+    fetchMovies(keyword);
+  }
+}
+
+function prevPage() {
+  if (currentPageNum > 1) {
+    currentPageNum--;
+    fetchMovies(document.getElementById("searchInput").value);
+  }
+}
+
+function nextPage() {
+  const maxPageNum = Math.ceil(totalMovies / moviesPerPage);
+  if (currentPageNum < maxPageNum) {
+    currentPageNum++;
+    fetchMovies(document.getElementById("searchInput").value);
+  }
+}
+
+function openMovieDetails(movie) {
+  const modal = document.getElementById("movieDetailsModal");
+  modal.innerHTML = `
+    <div class="modal-content">
+      <h2>${movie.Title}</h2>
+      <img src="${movie.Poster}" alt="${movie.Title}">
+      <label>Rate this movie:</label>
+      <input type="number" min="0" max="5" onchange="rateMovie('${movie.imdbID}', this.value)" value="${getMovieRating(movie.imdbID)}">
+      <div>
+        <h3>Comments</h3>
+        <ul>${getMovieComments(movie.imdbID).map((comment) => `<li>${comment}</li>`).join("")}</ul>
+        <input type="text" placeholder="Add a comment..." onkeyup="addComment(event, '${movie.imdbID}')">
+      </div>
+    </div>
+  `;
+
+  modal.style.display = "block";
+  selectedMovieId = movie.imdbID;
+}
+
+function closeMovieDetailsModal() {
+  const modal = document.getElementById("movieDetailsModal");
+  modal.style.display = "none";
+  selectedMovieId = null;
+}
+
+function getMovieRating(movieId) {
+  const rating = localStorage.getItem(`movie_rating_${movieId}`);
+  return rating ? Number(rating) : 0;
+}
+
+function rateMovie(movieId, rating) {
+  localStorage.setItem(`movie_rating_${movieId}`, rating);
+}
+
+function getMovieComments(movieId) {
+  const comments = localStorage.getItem(`movie_comments_${movieId}`);
+  return comments ? JSON.parse(comments) : [];
+}
+
+function addComment(event, movieId) {
+  if (event.key === "Enter") {
+    const comment = event.target.value;
+    const comments = getMovieComments(movieId);
+    comments.push(comment);
+    localStorage.setItem(`movie_comments_${movieId}`, JSON.stringify(comments));
+    openMovieDetails(currentMoviesData.find((movie) => movie.imdbID === movieId));
+  }
+}
+
+window.addEventListener("click", (event) => {
+  const modal = document.getElementById("movieDetailsModal");
+  if (event.target === modal) {
+    closeMovieDetailsModal();
+  }
+});
+
+// Initialize the app
+searchMovies();
